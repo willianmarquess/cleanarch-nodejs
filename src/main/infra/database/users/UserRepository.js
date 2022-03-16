b1e79b63f6337461fb9b9b546edb93288dc391bf
willianmarquess <willian.goncalves4@gmail.com>
2022-03-16 15:16:50 -0300

    adicionando o user repository utilizando banco de dados em memoria

---
+++ src/main/infra/database/users/UserRepository.js
@@ -0,0 +1,22 @@
+export default class UserRepository {
+    constructor (dbInstance) {
+        this.dbInstance = dbInstance
+    }
+
+    async save (user) {
+        this.dbInstance.push(user)
+        return await new Promise.resolve(user)
+    }
+
+    async findAll () {
+        return await new Promise.resolve(this.dbInstance)
+    }
+
+    async findById (id) {
+        return await new Promise.resolve(this.dbInstance.find((user) => user.id === id))
+    }
+
+    async findByEmail (email) {
+        return await new Promise.resolve(this.dbInstance.find((user) => user.email === email))
+    }
+}
