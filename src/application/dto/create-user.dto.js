a21c94bc7be7f6f741b261795edcf1c753bbd0b2
willianmarquess <willian.goncalves4@gmail.com>
2022-03-21 13:47:32 -0300

    tornando a dto create user validavel

--- src/application/dto/create-user.dto.js
+++ src/application/dto/create-user.dto.js
@@ -1,6 +1,14 @@
-export default class CreateUserDTO {
+import Validable from '../../main/shared/validation/validable.js'
+
+export default class CreateUserDTO extends Validable{
     constructor (email, password) {
         this.email = email
         this.password = password
+        this.validate()
+    }
+
+    validate () {
+        this.isEmail(this.email, 'email', 'e-mail inválid')
+        this.isMinLength(this.password, 10, 'password', 'the password must be at least 10 characters long')
     }
 }
